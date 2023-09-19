d685e9ebbf4b5f8cffa04e9137e0a2062c4e6470
cvoluj <berezanskiydaniiil@gmail.com>
2023-09-19 11:14:18 +0300

    removed model_dump and blank lines

--- app/core/config.py
+++ app/core/config.py
@@ -5,10 +5,14 @@ class ServerSettings(BaseSettings):
     HOST: str
     PORT: int
     RELOAD: bool
+    LOG_LEVEL: str
     
     model_config = SettingsConfigDict(env_file='.env')
 
 
 if __name__ == '__main__': # code snippet 
-    server_setting = ServerSettings().model_dump()
-    print(server_setting)
+    server_setting = ServerSettings()
+    print(server_setting.HOST)
+    print(server_setting.PORT)
+    print(server_setting.RELOAD)
+    print(server_setting.LOG_LEVEL)
--- app/main.py
+++ app/main.py
@@ -4,15 +4,13 @@ from routers import health
 from core.config import ServerSettings
 
 app = FastAPI()
-config = ServerSettings().model_dump()
-
-
-
+config = ServerSettings()
 app.include_router(health.router)
 
 
 if __name__ == "__main__":
-    host = config['HOST']
-    port = config['PORT']
-    reload = config['RELOAD']
-    uvicorn.run("main:app", host=host, port=port, log_level="info", reload=reload)
+    host = config.HOST
+    port = config.PORT
+    reload = config.RELOAD
+    log_level = config.LOG_LEVEL
+    uvicorn.run("main:app", host=host, port=port, log_level=log_level, reload=reload)
